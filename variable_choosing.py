d3a7dcce8f3fec128295ba49b539bef966e1d5a1
luaroncrew <ma-kir.ru@yandex.ru>
2023-06-02 19:19:43 +0200

    variables

---
+++ variable_choosing.py
@@ -0,0 +1,11 @@
+variables = [
+    'Coordonnées GPS de la formation',
+    'Effectif total des candidats pour une formation',
+    'Dont effectif des candidates admises',
+    'Effectif des admis en phase principale',
+    'Effectif des admis en phase complémentaire',
+    'Dont effectif des admis issus de la même académie',
+    'diploma_name',
+    'Filière de formation.1'
+]
+
